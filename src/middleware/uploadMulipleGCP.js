06172d698e400160774411e253bb13b00552cbdf
soyIsaacDev <86566954+soyIsaacDev@users.noreply.github.com>
2023-12-19 14:21:50 -0700

    GCP upload multiple images

--- src/middleware/uploadMulipleGCP.js
+++ src/middleware/uploadMulipleGCP.js
@@ -103,8 +103,62 @@ const multer = Multer({
     fileSize: 40 * 1024 * 1024 // no larger than 40mb
   }
 });
+
+// Custom file upload middleware
+const uploadImages = (req, res, next) => {
+  console.log("Upload Imagenes")
+  // Use multer upload instance
+  upload.array('imagenesfiles', 25)(req, res, (err) => {
+    if (err) {
+      console.log(err)
+      return res.status(400).json({ error: err.message });
+    }
+
+    // Retrieve uploaded files
+    const files = req.files;
+    const errors = [];
+    /* req.files.map(file => {
+      console.log("Mapping")
+      console.log(file)
+    }) */
+    //console.log(JSON.stringify(files));
+    const data = req.body;
+    //console.log("Image Data "+ JSON.stringify(data))
+    // Validate file types and sizes
+    files.forEach((file) => {
+      const allowedTypes = ['image/jpeg', 'image/png'];
+      const maxSize = 5 * 1024 * 1024; // 5MB
+
+      if (!allowedTypes.includes(file.mimetype)) {
+        errors.push(`Invalid file type: ${file.originalname}`);
+      }
+
+      if (file.size > maxSize) {
+        errors.push(`File too large: ${file.originalname}`);
+      }
+    });
+
+    // Handle validation errors
+    if (errors.length > 0) {
+      // Remove uploaded files
+      files.forEach((file) => {
+        fs.unlinkSync(file.path);
+      });
+
+      return res.status(400).json({ errors });
+    }
+
+    // Attach files to the request object
+    req.files = files;
+
+    // Proceed to the next middleware or route handler
+    next();
+  });
+};
+
 // [END multer]
 module.exports = {
   sendUploadToGCS,
-  multer
+  multer,
+  uploadImages
 };
