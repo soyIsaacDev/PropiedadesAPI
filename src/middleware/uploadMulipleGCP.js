06b0a6dc299cf7a3d381fd9d93233bddc797336d
soyIsaacDev <86566954+soyIsaacDev@users.noreply.github.com>
2023-12-20 18:54:06 -0700

    GCP upload multiple images 12

--- src/middleware/uploadMulipleGCP.js
+++ src/middleware/uploadMulipleGCP.js
@@ -125,18 +125,21 @@ const uploadImages = (req, res, next) => {
       console.log(file)
     }) */
     console.log("Req Files in Upload Imagenes "+JSON.stringify(req.files));
+    console.log("Files in uploadImages"+JSON.stringify(files));
     const data = req.body;
-    //console.log("Image Data "+ JSON.stringify(data))
+    console.log("Image Data "+ JSON.stringify(data))
     // Validate file types and sizes
     files.forEach((file) => {
       const allowedTypes = ['image/jpeg', 'image/png'];
       const maxSize = 5 * 1024 * 1024; // 5MB
 
       if (!allowedTypes.includes(file.mimetype)) {
+        console.log(`Invalid file type`)
         errors.push(`Invalid file type: ${file.originalname}`);
       }
 
       if (file.size > maxSize) {
+        console.log("File too large")
         errors.push(`File too large: ${file.originalname}`);
       }
     });
@@ -152,6 +155,7 @@ const uploadImages = (req, res, next) => {
     }
 
     // Attach files to the request object
+    console.log("Files to Attach to req.files " +files)
     req.files = files;
 
     // Proceed to the next middleware or route handler
