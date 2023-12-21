1d53f0ef20906801acd9f4f365f88c6aedb06ce2
soyIsaacDev <86566954+soyIsaacDev@users.noreply.github.com>
2023-12-20 17:37:58 -0700

    GCP upload multiple images 7

--- src/routes/propiedad.js
+++ src/routes/propiedad.js
@@ -18,7 +18,7 @@ const { Propiedad, ImgPropiedad, AmenidadesDesarrollo, AmenidadesPropiedad,Tipod
 uploadImgPropiedad.uploadImagenPropiedad ) */;
 
 server.post("/nuevaPropiedad", 
-  gcpImageUpload.multer.single('imagenesfiles'),
+  gcpImageUpload.multer.array('imagenesfiles', 25),
   gcpImageUpload.sendUploadToGCS,
   async (req, res) => {
     try {
