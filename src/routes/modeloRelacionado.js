4b08b5d864a440621d233f807543f63186e191f7
soyIsaacDev <86566954+soyIsaacDev@users.noreply.github.com>
2024-05-14 18:31:11 -0700

    Crear ModeloRelacionado con ImagenModelo Cambio de Tamaño debugg B

--- src/routes/modeloRelacionado.js
+++ src/routes/modeloRelacionado.js
@@ -32,7 +32,7 @@ server.get("/getAllDataandImagenModeloAsociadoPropiedad", async (req, res) => {
       include: [
         {
           model: ImgModeloAsociado,
-          attributes: ['img_name'],
+          attributes: ['img_name','thumbnail_img','detalles_imgGde','detalles_imgChica'],
         }
       ]
     },);
