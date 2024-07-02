db52184500a943ad1382ceb5feeaf34e071920f6
soyIsaacDev <86566954+soyIsaacDev@users.noreply.github.com>
2024-07-01 18:06:11 -0700

    Editar Propiedad

--- src/controllers/editarImgPropiedadLocal.js
+++ src/controllers/editarImgPropiedadLocal.js
@@ -48,57 +48,60 @@ const editarPropiedad = async (req, res, next) => {
       // buscamos si hay fotos
       const files = req.files;
 
-      if (files === undefined) {
-        console.log("Selecciona una imagen para tu propiedad")
-        //return res.send(`Selecciona una imagen para tu propiedad`);
-      }
+      // Si hay imagenes nuevas cargadas
+      if(files.length>0) {
 
-      console.log("Files en creacion de Instancia " + JSON.stringify(files))
-      for (let i = 0; i < ordenImagen.length; i++) {
-        if(ordenImagen[i].editar===1){ // 1 = editar
-          const imagenPropiedad = await ImgPropiedad.findByPk(ordenImagen[i].id);
-          console.log("Orden Editado " +JSON.stringify(imagenPropiedad))
-          imagenPropiedad.orden= ordenImagen[i].orden
-          await imagenPropiedad.save();
+        console.log("Files en creacion de Instancia " + JSON.stringify(files))
+        for (let i = 0; i < ordenImagen.length; i++) {
+          if(ordenImagen[i].editar===1){ // 1 = editar
+            const imagenPropiedad = await ImgPropiedad.findByPk(ordenImagen[i].id);
+            console.log("Orden Id "+ordenImagen[i].id +  " Editar "+ ordenImagen[i].editar+  " Orden Editado " +JSON.stringify(imagenPropiedad))
+            imagenPropiedad.orden= ordenImagen[i].orden
+            await imagenPropiedad.save();
+          }
+          else if (ordenImagen[i].editar===2){ // 2 = borrar
+            const imagenPropiedad = await ImgPropiedad.findByPk(ordenImagen[i].id);
+            await imagenPropiedad.destroy();
+            fs.unlink(carpeta+"/"+ordenImagen[i].img_name, (err) => {
+                if (err) {
+                    throw err;
+                }
+                console.log("Delete File successfully " + ordenImagen[i].img_name);
+              });
+          }
         }
-        else if (ordenImagen[i].editar===2){ // 2 = borrar
-          const imagenPropiedad = await ImgPropiedad.findByPk(ordenImagen[i].id);
-          await imagenPropiedad.destroy();
-          fs.unlink(carpeta+"/"+ordenImagen[i].thumbnail_img, (err) => {
-              if (err) {
-                  throw err;
-              }
-              console.log("Delete File successfully " + ordenImagen[i].thumbnail_img);
+        // se crea una imagen por cada archivo y se liga a la Propiedad
+        files.forEach(async (file) => {
+          //console.log("Image File " + JSON.stringify(file));
+          ordenImagen.forEach((imagen) =>{
+            
+            console.log("Orden Data Image Name "+imagen.img_name + " Image File " + file.originalname)
+          })
+          const ordenData = ordenImagen.filter((imagen)=>imagen.img_name === file.originalname);
+          
+          //console.log("Orden Data "+JSON.stringify(ordenImagen))
+            const nombre_imagen = file.filename.slice(0, file.filename.length - 4);
+            const imagenPropiedad = await ImgPropiedad.create({
+              orden:ordenData[0].orden,
+              type: file.mimetype,
+              img_name: file.filename,
+              thumbnail_img:"Thumbnail_WebP_"+nombre_imagen+".webp",
+              detalles_imgGde:"Detalles_Img_Gde_"+nombre_imagen+".webp",
+              detalles_imgChica:"Detalles_Img_Chica_"+nombre_imagen+".webp",
+              PropiedadId: PropiedadBuscada.id
             });
-        }
+        })
       }
-      // se crea una imagen por cada archivo y se liga a la Propiedad
-      files.forEach(async (file) => {
-        console.log("Image File " + JSON.stringify(file));
-
-        const ordenData = ordenImagen.filter((imagen)=>imagen.imageName === file.originalname);
-        
-        console.log("Orden Data "+JSON.stringify(ordenData))
-          const nombre_imagen = file.filename.slice(0, file.filename.length - 4);
-          const imagenPropiedad = await ImgPropiedad.create({
-            orden:ordenData[0].orden,
-            type: file.mimetype,
-            img_name: file.filename,
-            thumbnail_img:"Thumbnail_WebP_"+nombre_imagen+".webp",
-            detalles_imgGde:"Detalles_Img_Gde_"+nombre_imagen+".webp",
-            detalles_imgChica:"Detalles_Img_Chica_"+nombre_imagen+".webp",
-            PropiedadId: PropiedadBuscada[0].id
-          });
-      })
-
+      else console.log("No hay imagenes nuevas a cargar");
+      
       //res.json(`Se creo la Propiedad `+ PropiedadBuscada[0].nombrePropiedad +  " y sus imagenes " );
       console.log("Se Edito la Propiedad");
       const propCreadaJSON = {
-        Confirmacion:`Se edito la Propiedad `+ PropiedadBuscada.dataValues.nombreDesarrollo
+        Confirmacion:`Se edito la Propiedad `+ PropiedadBuscada.nombreDesarrollo
       }
       res.json(propCreadaJSON? propCreadaJSON :{mensaje:"No Se pudo crear la propieda"} );
     } catch (error) {
-      console.log("Error en Upload Multiple Img "+error);
+      console.log("Error al editar la imagen "+error);
       //res.json(`Error al intentar crear la imagen de la propiedad: ${error}`);
     }
   };
