2a08d6171f8ef72cc3bf42965d8b7d799429421b
soyIsaacDev <86566954+soyIsaacDev@users.noreply.github.com>
2024-08-19 10:30:32 -0700

    Upload Imagenes de hasta 10 MB

--- src/middleware/uploadImagenesGCPModelo.js
+++ src/middleware/uploadImagenesGCPModelo.js
@@ -35,7 +35,7 @@ const uploadModeloImages = (req, res, next) => {
     // Validate file types and sizes
     files.forEach((file) => {
       const allowedTypes = ['image/jpeg', 'image/png'];
-      const maxSize = 5 * 1024 * 1024; // 5MB
+      const maxSize = 10 * 1024 * 1024; // 10MB
 
       if (!allowedTypes.includes(file.mimetype)) {
         console.log(`Invalid file type`)
@@ -51,14 +51,14 @@ const uploadModeloImages = (req, res, next) => {
     // Handle validation errors
     if (errors.length > 0) {
       // Remove uploaded files
-      files.forEach((file) => {
+      /* files.forEach((file) => {
         fs.unlinkSync(file.path, (err) => {
           if (err) {
               throw err;
           }
           console.log("Delete File successfully.");
         });
-      });
+      }); */
 
       return res.status(400).json({ errors });
     }
