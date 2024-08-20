4a3938d1a6f46863f0719fa47ed9d16c727b5d0a
soyIsaacDev <86566954+soyIsaacDev@users.noreply.github.com>
2024-08-20 11:08:37 -0700

    Borrar imagenes al editar propiedad A

--- src/routes/imgPropiedad.js
+++ src/routes/imgPropiedad.js
@@ -4,7 +4,7 @@ const express = require("express");
 const path = require('path');
 var public = path.join(__dirname, '../../uploads');
 var googleCloudStorage = "https://storage.cloud.google.com/dadinumco-media"
-const {  ImgPropiedad} = require("../db");
+const { ImgPropiedad, ImgModeloAsociado } = require("../db");
 
 server.use('/', express.static(public));
 
@@ -18,7 +18,56 @@ server.get("", async (req,res)=> {
     catch(e){
       res.send(e);
     }
-  });
+});
+
+server.get("/imagenDesarrollo/:imagenId", async (req,res)=> {
+  try{
+    let {imagenId} = req.params;
+    const imagen = await ImgPropiedad.findByPk(imagenId);
+    res.send(imagen)
+  }
+  catch(e){
+    res.send(e);
+  }
+});
+
+server.get("/imagenModelo/:imagenId", async (req,res)=> {
+  try{
+    let {imagenId} = req.params;
+    const imagen = await ImgModeloAsociado.findByPk(imagenId);
+    res.send(imagen)
+  }
+  catch(e){
+    res.send(e);
+  }
+});
+
+server.get("/imagenDesarrolloAll/:desarrolloId", async (req,res)=> {
+  try{
+    let {desarrolloId} = req.params;
+    const imagenDesarrollo = await ImgPropiedad.findAll({
+      where:{PropiedadId:desarrolloId}
+    });
+    res.send(imagenDesarrollo)
+  }
+  catch(e){
+    res.send(e);
+  }
+});
+
+server.get("/imagenModeloAll/:modeloId", async (req,res)=> {
+  try{
+    let {modeloId} = req.params;
+    const imagenModelo = await ImgModeloAsociado.findAll({
+      where:{ModeloAsociadoPropiedadId:modeloId}
+    });
+    res.send(imagenModelo)
+  }
+  catch(e){
+    res.send(e);
+  }
+});
+
 
 
 module.exports =  server;
