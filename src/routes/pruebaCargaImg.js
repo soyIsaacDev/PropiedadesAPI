508580b33626f501500891b94c3322419be40057
soyIsaacDev <86566954+soyIsaacDev@users.noreply.github.com>
2024-10-16 09:54:59 -0700

    Pruebas Upload Img GCP 5

--- src/routes/pruebaCargaImg.js
+++ src/routes/pruebaCargaImg.js
@@ -6,8 +6,15 @@ const {pruebaUploadModeloImages, pruebaSendModeloUploadToGCSAsync} = require('..
 
 
 // Modelo
-server.post("/nuevoModeloAsociadoPropiedad", pruebaUploadModeloImages); 
+//server.post("/nuevoModeloAsociadoPropiedad", pruebaUploadModeloImages); 
 //server.post("/nuevoModeloAsociadoPropiedad", pruebaUploadModeloImages, pruebaSendModeloUploadToGCSAsync); 
+
+server.post("/nuevoModeloAsociadoPropiedad", async (req,res)=>{
+  res.json({
+    codigo:1, 
+    Mensaje:`Si pasaron las imagenes`
+  });
+});
 
 server.get("/funcionando", async (req,res)=>{
   res.json("Funcionando OK")
